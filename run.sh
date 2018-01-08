d4098178ed104312c783c956f49b4a985eb9ec57
Matti Andreas Nielsen <mattinielsen5@hotmail.com>
2018-01-08 21:33:37 +0100

    some foreverjs bug

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/www/denlillemand.com/blog --port=80 --dbuser=denlillemand --dbname=blog --static-dir='/home/www/denlillemand.com/static/' --template-dir='/home/www/denlillemand.com/templates/'
+/home/www/denlillemand.com/blog --port=80 --dbuser=denlillemand --dbname=blog --static-dir="/home/www/denlillemand.com/static/" --template-dir="/home/www/denlillemand.com/templates/"
