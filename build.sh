0d77115a2a800a54741c22235cf18ae9dcf44bba
Matti Andreas Nielsen <mattinielsen5@hotmail.com>
2018-01-09 19:16:30 +0100

    Debugging the test coverage badge even more

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ webpack
 go build
 cp ../../../../bin/goose ./
 cp -rf dist/* static/
-go test -cover | grep % > test_coverage
+go test -cover | grep % > test_coverage.txt
 node test_coverage.js
 rm test_coverage
--- test_coverage.js
+++ test_coverage.js
@@ -2,20 +2,25 @@ var fs = require('fs');
 const badge = require('gh-badges')
 
 fs.readFile('test_coverage.txt', (err, data) => {
-  if (err) throw err;
-    var splitedTestCov = data.split(':');
-    //TODO maybe change the color scheme to red if below like 80%
-    const format = {
-      text: [splitedTestCov[0], splitedTestCov[1]],
-      colorscheme: 'green',
-      template: 'flat',
-    }
-    badge.loadFont('/usr/share/fonts/TTF/Hack-Regular.ttf', err => {
-      badge(format, (svg, err) => {
-	// svg is a string containing your badge
-	fs.writeFile('testcoverage.svg', svg, (err) => {
-	  if (err) throw err;
-	  console.log('The file has been saved!');
-	});
-      })})
+  if (err) {
+   throw err;   
+  }
+  console.log(data);
+  var splitedTestCov = data.toString().split(':');
+  //TODO maybe change the color scheme to red if below like 80%
+  const format = {
+    text: [splitedTestCov[0], splitedTestCov[1]],
+    colorscheme: 'green',
+    template: 'flat',
+  }
+  badge.loadFont('/usr/share/fonts/TTF/Hack-Regular.ttf', err => {
+    badge(format, (svg, err) => {
+    // svg is a string containing your badge
+    fs.writeFile('testcoverage.svg', svg, (err) => {
+      if (err) {
+	throw err;  
+      }
+      console.log('The test_coverage.svg file has been saved. \n');
+    });
+  })})
 });
